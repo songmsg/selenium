f743ac3f3a532c78ff3752601e4f33b0b22f4e49
Wenying Song <wenying.song@msg.group>
2023-01-11 16:24:31 +0100

    selenium

---
+++ src/test/java/com/agcs/arp/des/selenium/integrationTests/EdiTest.java
@@ -0,0 +1,53 @@
+package com.agcs.arp.des.selenium.integrationTests;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class EdiTest {
+
+    static final Logger log = getLogger(lookup().lookupClass());
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    void test() {
+        String sutUrl = "https://bonigarcia.dev/selenium-webdriver-java/";
+        driver.get(sutUrl);
+        String title = driver.getTitle();
+        log.debug("The title of {} is {}", sutUrl, title);
+        assertThat(title).contains("Selenium WebDriver");
+    }
+
+}
